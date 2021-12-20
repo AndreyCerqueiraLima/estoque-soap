f80d5a205873f848c7d0cbfdc61ba18e59e9c180
AndreyCerqueiraLima <andrey.cerqueira.work@gmail.com>
2021-12-20 14:22:39 -0300

    First Commit

---
+++ src/main/java/br/com/caelum/escola/ws/AlunoWs.java
@@ -0,0 +1,29 @@
+package br.com.caelum.escola.ws;
+
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
+import br.com.caelum.escola.ws.model.ListaAlunos;
+import br.com.caelum.escola.ws.repository.AlunoRepository;
+
+@WebService
+public class AlunoWs {
+	
+	private AlunoRepository repository;
+	
+	public AlunoWs() {
+		this.repository = new AlunoRepository();
+	}
+	
+	@WebMethod(operationName = "alunos")
+	@WebResult(name = "alunos", targetNamespace = "")
+	public ListaAlunos findAll(){
+		ListaAlunos listaAlunos = new ListaAlunos();
+		listaAlunos.setAluno( this.repository.findAll());
+		return listaAlunos;
+	}
+	
+}
