74af622e969e0060b4f8e0f513467004aafd09cb
AndreyCerqueiraLima <andrey.cerqueira.work@gmail.com>
2021-12-20 15:14:46 -0300

    Adicionando primeiro teste basicao

--- src/test/java/estoque/TesteIntegrationAlunoTest.java
+++ src/test/java/estoque/TesteIntegrationAlunoTest.java
@@ -1,10 +1,13 @@
 package estoque;
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class TesteIntegrationAlunoTest {
 	@Test
 	public void findAllIntegration() {
+		assertEquals("oi", "oi");
 	}
 }
